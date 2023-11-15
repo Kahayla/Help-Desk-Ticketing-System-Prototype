2c21ec0f921343ca0afd47a7da437cc31dd6fa9e
Kahayla Walsh <kahayla.walsh@developersinstitute.co.nz>
2023-11-15 20:59:33 +1300

    base app created

---
+++ main.py
@@ -0,0 +1,4 @@
+print("Please select one of the following options: \n0: Exit \n1: Submit helpdesk ticket \n2: Show all tickets \n3: Respond to ticket \n4: Re-open resolved ticket \n5: Show ticket statistics")
+
+exit = input("What would you like to do? ")
+print(exit)
