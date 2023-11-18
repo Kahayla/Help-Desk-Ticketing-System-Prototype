b43009cf4d8649f3b49f8d61e7bcd04f8d3745b3
Kahayla Walsh <kahayla.walsh@developersinstitute.co.nz>
2023-11-18 22:02:17 +1300

    fixed the logging of each helpdesk ticket by using the .__string__ function

--- main.py
+++ main.py
@@ -12,6 +12,11 @@ class Tickets:
         Tickets.ticket_counter += 1
         Tickets.all_tickets.append(self)
         
+    def __str__(self):
+        return f"Ticket ID: {self.ticketID}\nName: {self.name}\nEmail: {self.email}\nStaff ID: {self.staffID}\nDescription: {self.description}\nStatus: {self.status}\n"
+    
+    def __repr__(self):
+        return self.__str__()
 
 def submit_ticket():
     print("Submit a ticket")
@@ -29,37 +34,37 @@ def submit_ticket():
 def show_all_tickets():
     print("Showing all tickets...")
     for ticket in Tickets.all_tickets:
-        print("Ticket ID: {}\nName: {}\nEmail: {}\nStaff ID: {}\nDescription: {}\nStatus: {}\n".format(
-            ticket.ticketID, ticket.name, ticket.email, ticket.staffID, ticket.description, ticket.status
-        ))
-print("Please select one of the following options: \n0: Exit \n1: Submit helpdesk ticket \n2: Show all tickets \n3: Respond to ticket \n4: Re-open resolved ticket \n5: Show ticket statistics")
-
-user_input = input("What would you like to do? ")
-
-
-
-if user_input == "0":
-    print("Exiting...")
-    quit()
-
-if user_input == "1":
-    print("Submitting helpdesk ticket...")
-
-if user_input == "2":
-    print("Getting all tickets...")
-    print(Tickets.all_tickets)
-
-if user_input == "3":
-    print("Responding to ticket...")
-    
-if user_input == "4":
-    print("Re-opening resolved ticket...")
-    
-if user_input == "5":
-    print("Showing ticket statistics...")
-
-if user_input == "help":
+        print(ticket)
+        
+while True:
     print("Please select one of the following options: \n0: Exit \n1: Submit helpdesk ticket \n2: Show all tickets \n3: Respond to ticket \n4: Re-open resolved ticket \n5: Show ticket statistics")
-    
-else :
-    print("Please select a number from one of options above if you would like reminder please type 'help'")
+
+    user_input = input("What would you like to do? ")
+
+
+
+    if user_input == "0":
+        print("Exiting...")
+        quit()
+
+    if user_input == "1":
+        submit_ticket()
+
+    if user_input == "2":
+        print("Getting all tickets...")
+        print(Tickets.all_tickets)
+
+    if user_input == "3":
+        print("Responding to ticket...")
+        
+    if user_input == "4":
+        print("Re-opening resolved ticket...")
+        
+    if user_input == "5":
+        print("Showing ticket statistics...")
+
+    if user_input == "help":
+        print("Please select one of the following options: \n0: Exit \n1: Submit helpdesk ticket \n2: Show all tickets \n3: Respond to ticket \n4: Re-open resolved ticket \n5: Show ticket statistics")
+        
+    else :
+        print("Please select a number from one of options above if you would like reminder please type 'help'")
