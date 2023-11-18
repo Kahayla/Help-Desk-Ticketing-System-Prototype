7801ea17ae47c963188c1733eed1e74f54fcea8b
Kahayla Walsh <kahayla.walsh@developersinstitute.co.nz>
2023-11-18 21:51:19 +1300

    refactored functions to make them easier to reuse

--- main.py
+++ main.py
@@ -1,9 +1,35 @@
 class Tickets:
     all_tickets = []
+    ticket_counter = 2000
+    
+    def __init__(self, name, email, staffID, description):
+        self.name = name
+        self.email = email
+        self.staffID = staffID
+        self.description = description
+        self.status = "Open"
+        self.ticketID = Tickets.ticket_counter
+        Tickets.ticket_counter += 1
+        Tickets.all_tickets.append(self)
+        
 
+def submit_ticket():
+    print("Submit a ticket")
+    name = input("What is your name? ")
+    email = input("What is your email? ")
+    staffID = input("What is your staff ID? ")
+    description = input("Please describe your issue: ")
+    
+    new_ticket = Tickets(name, email, staffID, description)
+    print("Ticket Summary - \nName: {}\nEmail: {}\nStaff ID: {}\nDescription: {}".format(
+        new_ticket.name, new_ticket.email, new_ticket.staffID, new_ticket.description
+    ))
+    print("Ticket submitted successfully.")
 print("Please select one of the following options: \n0: Exit \n1: Submit helpdesk ticket \n2: Show all tickets \n3: Respond to ticket \n4: Re-open resolved ticket \n5: Show ticket statistics")
 
 user_input = input("What would you like to do? ")
+
+
 
 if user_input == "0":
     print("Exiting...")
@@ -11,25 +37,6 @@ if user_input == "0":
 
 if user_input == "1":
     print("Submitting helpdesk ticket...")
-    name = input("What is your name? ")
-    email = input("What is your email? ")
-    staffID = input("What is your staff ID? ")
-    description = input("What is your issue? ")
-    print("Ticket Summary - \nName: " + name + "\nEmail: " + email + "\nStaff ID: " + staffID + "\nDescription: " + description)
-    class Ticket:
-        def __init__(self, name, email, staffID, description):
-            self.name = name
-            self.email = email
-            self.staffID = staffID
-            self.description = description
-            self.status = "Open"
-            self.ticketID = len(Tickets.all_tickets) + 1
-            
-            
-            Tickets.all_tickets.append(self)
-    
-    print("Ticket submitted successfully.")
-    user_input = input("What would you like to do? ")
 
 if user_input == "2":
     print("Getting all tickets...")
