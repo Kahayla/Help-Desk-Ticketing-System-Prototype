ffbe6afcd84a006d6eddbf3a39cc69da592c82e2
Kahayla Walsh <kahayla.walsh@developersinstitute.co.nz>
2023-11-18 22:27:34 +1300

    added different log in options to seperate Users from IT team members

--- main.py
+++ main.py
@@ -52,35 +52,37 @@ def respond_to_ticket():
     else:
         print("Ticket" + ticket_id + "does not exist.")
     
-        
-while True:
-    print("Please select one of the following options: \n0: Exit \n1: Submit helpdesk ticket \n2: Show all tickets \n3: Respond to ticket \n4: Re-open resolved ticket \n5: Show ticket statistics")
 
-    user_input = input("What would you like to do? ")
-
-
-
-    if user_input == "0":
-        print("Exiting...")
-        quit()
-
-    if user_input == "1":
-        submit_ticket()
-
-    if user_input == "2":
-        show_all_tickets()
-
-    if user_input == "3":
-        respond_to_ticket()
-        
-    if user_input == "4":
-        print("Re-opening resolved ticket...")
-        
-    if user_input == "5":
-        print("Showing ticket statistics...")
-
-    if user_input == "help":
+role = input("Please type select role you are: \n1: User  \n2: IT team member \nWhat role are you?")
+if role == "1":     
+    while True:
         print("Please select one of the following options: \n0: Exit \n1: Submit helpdesk ticket \n2: Show all tickets \n3: Respond to ticket \n4: Re-open resolved ticket \n5: Show ticket statistics")
-        
-    else :
-        print("Please select a number from one of options above if you would like reminder please type 'help'")
+
+        user_input = input("What would you like to do? ")
+
+        if user_input == "0":
+            print("Exiting...")
+            quit()
+
+        if user_input == "1":
+            submit_ticket()
+
+        if user_input == "2":
+            show_all_tickets()
+
+        if user_input == "3":
+            respond_to_ticket()
+            
+        if user_input == "4":
+            print("Re-opening resolved ticket...")
+            
+        if user_input == "5":
+            print("Showing ticket statistics...")
+
+        if user_input == "help":
+            print("Please select one of the following options: \n0: Exit \n1: Submit helpdesk ticket \n2: Show all tickets \n3: Respond to ticket \n4: Re-open resolved ticket \n5: Show ticket statistics")
+            
+        else :
+            print("Please select a number from one of options above if you would like reminder please type 'help'")
+elif role == "2":
+    print("IT member")
