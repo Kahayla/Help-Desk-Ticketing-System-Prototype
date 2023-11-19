6385374f2c9df238dff56fa07f4c021deb2b5c74
Kahayla Walsh <kahayla.walsh@developersinstitute.co.nz>
2023-11-19 18:33:07 +1300

    made name and email global variables to make for a better user experience and also added formatting and line breaks where appropriate to make the app more readable

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+# creating a ticket class with a property of all_tickets to store every ticket that is created and ticket_counter to generate a unique id for each ticket
 class Tickets:
     all_tickets = []
     ticket_counter = 2000
 
+#creating attributes/schema for each ticket in the all_tickets array 
     def __init__(self, name, email, staffID, description):
         self.name = name
         self.email = email
@@ -14,44 +16,42 @@ class Tickets:
         Tickets.ticket_counter += 1
         Tickets.all_tickets.append(self)
 
+#some fancy stringify function to make tickets readable in the console
     def __str__(self):
         return f"Ticket ID: {self.ticketID}\nName: {self.name}\nEmail: {self.email}\nStaff ID: {self.staffID}\nDescription: {self.description}\nStatus: {self.status}\nUser Responses: {self.user_responses}\nInternal Responses: {self.internal_responses}"
 
     def __repr__(self):
         return self.__str__()
-
-def submit_ticket(staffID):
-    print("Submit a ticket")
-    name = input("What is your name? ")
-    email = input("What is your email? ")
+    
+# defining the submit_ticket function with a parameter of staffID to prevent repition at the user's end
+def submit_ticket(staffID, name, email):
+    print("\nSubmit a ticket")
+    # setting the description parameter to be a user input
     description = input("Please describe your issue: ")
-
     new_ticket = Tickets(name, email, staffID, description)
-    print("Ticket Summary - \nName: {}\nEmail: {}\nStaff ID: {}\nDescription: {}".format(
+    print("\nTicket Summary - \nName: {}\nEmail: {}\nStaff ID: {}\nDescription: {}".format(
         new_ticket.name, new_ticket.email, new_ticket.staffID, new_ticket.description
     ))
-    print("Ticket submitted successfully.")
-
-    # Check if the description contains "password change" and apply the logic
+    print("Ticket submitted successfully.\n")
     if "password change" in description.lower():
         new_password = staffID[:2] + name[:3]
         response = "Here is your new password: " + new_password
         new_ticket.user_responses.append(response)
         new_ticket.status = "Resolved"
 
-def show_all_tickets_created_by_user(staffID):
-    print("Showing all tickets...")
+def show_all_tickets_created_by_user(staff_id):
+    print("\nShowing all tickets...\n")
     for ticket in Tickets.all_tickets:
-        if ticket.staffID == staffID:
-            print("Ticket ID:", ticket.ticketID)
-            print("Description:", ticket.description)
+        if ticket.staffID == staff_id:
+            print(f"Ticket ID: {ticket.ticketID}")
+            print(f"Description: {ticket.description}")
             
             if not ticket.user_responses:
                 print("Responses: No response yet")
             else:
-                print("Responses:", ticket.user_responses)
+                print(f"Responses: {ticket.user_responses}")
             
-            print("Status:", ticket.status)
+            print(f"Status: {ticket.status}\n")
 
 def update_ticket():
     ticket_id = int(input("What is the ID of the ticket you would like to update? "))
@@ -94,25 +94,25 @@ def update_ticket():
         
 def show_all_tickets():
     for ticket in Tickets.all_tickets:
-        print(ticket)
+        print(f"{ticket}\n")
     
 def user_respond_to_ticket():
-    print("Responding to ticket...")
-    ticket_id = int(input("What is the ticket ID? "))
-    response = input("What is your response? ")
+    print("\nResponding to ticket...")
+    ticket_id = int(input("\nWhat is the ticket ID? "))
     for ticket in Tickets.all_tickets:
         if ticket.ticketID == ticket_id:
+            response = input("What is your response? ")
             ticket.user_responses.append(response)
-            print("Response added successfully.")
+            print("\nResponse added successfully.\n")
             print("Ticket Summary - \nName: {}\nEmail: {}\nStaff ID: {}\nDescription: {}\nResponses: {}".format(
                 ticket.name, ticket.email, ticket.staffID, ticket.description, ticket.user_responses
             ))
             break
     else:
-        print(f"Ticket {ticket_id} does not exist.")
+        print(f"\nTicket {ticket_id} does not exist.\n")
 
 def check_open_tickets():
-    print("Grabbing open tickets")
+    print("\nGrabbing open tickets")
     for ticket in Tickets.all_tickets:
         if ticket.status == "Open":
             print(ticket)
@@ -130,13 +130,13 @@ def show_ticket_dashboard():
         if not ticket.user_responses:
             total_tickets_with_no_response += 1
     
-    print(f"Ticket Statistics \nTickets created: {total_tickets} \nTickets not resolved: {total_tickets_to_resolve} \nTickets resolved: {total_tickets_resolved}\n Total tickets with no response {total_tickets_with_no_response}")
+    print(f"\nTicket Statistics \nTickets created: {total_tickets} \nTickets not resolved: {total_tickets_to_resolve} \nTickets resolved: {total_tickets_resolved}\nTotal tickets with no response {total_tickets_with_no_response}\n")
     print("Do you want to see the list of tickets?")
-    see_all_tickets = input("Y or N").lower()
+    see_all_tickets = input("Y or N ").lower()
     
     if see_all_tickets == "y":
         for ticket in Tickets.all_tickets:
-            print(ticket)
+            print(f"\n{ticket}\n")
         
     
 
@@ -144,14 +144,18 @@ def main():
     while True:
         role = None
         staff_id = None
-        print(f"Role: {role}, Staff ID: {staff_id}")
-        print("LOG IN")
-        role = input("Please type select role you are: \n1: User  \n2: IT team member \nWhat role are you?")
-        staff_id = input("What is your staff_id?")
+        name = None
+        email = None
+        # print(f"Role: {role}, Staff ID: {staff_id}")
+        print("LOG IN\n")
+        role = input("Please type select role you are: \n1: User  \n2: IT team member \nWhat role are you? ")
+        staff_id = input("What is your staff_id? ")
+        name = input("What is your name? ")
+        email = input("What is your email? ")
 
         if role == "1":
             while True:
-                print("Please select one of the following options: \n0: Log out \n1: Submit helpdesk ticket \n2: Show all tickets \n3: Respond to ticket")
+                print("\n----------------------\nPlease select one of the following options: \n0: Log out \n1: Submit helpdesk ticket \n2: Show all tickets \n3: Respond to ticket\n")
 
                 user_input = input("What would you like to do? ")
 
@@ -160,7 +164,7 @@ def main():
                     break
 
                 if user_input == "1":
-                    submit_ticket(staff_id)
+                    submit_ticket(staff_id, name, email)
 
                 if user_input == "2":
                     show_all_tickets_created_by_user(staff_id)
@@ -176,7 +180,7 @@ def main():
 
         elif role == "2":
             while True:
-                print("Please select one of the following options: \n0: Log out \n1: Check all Open tickets \n2: Check all tickets\n3: Update ticket \n4: Go to ticket dashboard")
+                print("\nPlease select one of the following options: \n0: Log out \n1: Check all Open tickets \n2: Check all tickets\n3: Update ticket \n4: Go to ticket dashboard\n")
 
                 user_input = input("What would you like to do? ")
 
